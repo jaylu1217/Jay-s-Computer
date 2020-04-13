14ad0d6971ca65cd83ffb8d0c9be14655b193345
jaylu1217 <63581563+jaylu1217@users.noreply.github.com>
2020-04-13 21:30:14 +0800

    Add files via upload

---
+++ Capstone Project.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#This notebook is mainly used for the Data Science Capstone Project
+
+
+# In[2]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[3]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
